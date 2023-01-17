92fcb54bd48af6c8d4e74a568ec13b23697f55eb
Alex Jilkin <jilkinalex@gmail.com>
2023-01-17 20:52:37 +0200

    Add harmonic bunch

--- week1/harmonic.cpp
+++ week1/harmonic.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
 #include <math.h>
 
-float harmonic_series () {
+float harmonic() {
   float sum = 0;
+  float prevSum = 0;
+  double k = 1;
 
-  for (int k = 1; k < 1000000000; k++) {
+  while(true) {
     sum += (1 / (float)k);
+
+    if (sum == prevSum) {
+      return sum;
+      std::cout << k;
+    }
+
+    prevSum = sum;
+    k++;
   }
   
-  return sum;
+  return -1;
 }
+
+float harmonic_bunch (int N) {
+  float sum = 0;
+  float prevSum = 0;
+  double k = 1;
+
+  while(true) {
+
+    for (int j = 0; j < N; j++) {
+      sum += (1 / (float)k);
+    }
+
+    if (sum == prevSum) {
+      return sum;
+      std::cout << k;
+    }
+
+    prevSum = sum;
+    k++;
+  }
+  
+  return -1;
+}
+
+
 
 int main() {
    
-    std::cout << harmonic_series();
+    std::cout << harmonic_bunch(50);
 
     std::cout << "\n";
 }
