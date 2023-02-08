12083194452710c828039edbf32cb63f2a043482
Alex Jilkin <jilkinalex@gmail.com>
2023-02-08 21:38:59 +0200

    implement newton root finding, and print example for complex roots of polynomial using eigen values method

--- week4/complexroots.py
+++ week4/complexroots.py
@@ -1,21 +1,29 @@
 import numpy as np
-
-import math
-
 LA = np.linalg
 
+# p ---> p[0] * x**n + p[1] * x**(n-1) + ... + p[n-1]*x + p[n]
+# p starts with the ^n term
 def myroots(N, p):
-  # TODO: should be not identity
+
+  # Creates the needed matrix A, adds the polynomial as the first row.
   A = np.identity(N - 1).tolist()
   ex = [-(p[i + 1] / p[0]) for i in range(0, N)]
-
   A.insert(0, ex)
+
+  # Adds a column of zeros
   for i in range(1, N):
     A[i].append(0)
 
   print(A)
   w, v = LA.eig(A)
 
-  print(w)
-  
-myroots(2, [1, 6, 9])
+  return w
+
+# The first one should have 3
+polynomials = [[1, 6, 9], [5, 5, 5, 7, 8], [14, 15, 16, 17]]
+for p in polynomials:
+  roots = myroots(len(p) - 1, p)
+  np_roots = np.roots(p)
+
+  print ("for p={} \n myroots(p)={} \n np.roots(p)={}".format(p, roots, np_roots))
+
--- week4/roots.py
+++ week4/roots.py
@@ -7,7 +7,7 @@ def f(x):
   return np.sin((tr_pi * x**3) / (x**2 - 1)) + 1/2
 
 def f_prime(x):
-  return np.sin((tr_pi * x**3) / (x**2 - 1)) + 1/2
+  return (tr_pi * np.cos((tr_pi * x**3) / (x**2 - 1)) * (x**4 - 3*(x**2))) / ((x**2 - 1)* (x**2 - 1))
 
 tol = 0.00001
 
@@ -24,8 +24,18 @@ def bisect_f(a, b):
   return bisect_f(a, m) if f_m * f_a < 0 else bisect_f(m, b)
 
 def newton_f(x0):
+  x_i = x0 - (f(x0) / f_prime(x0))
   
+  if (np.abs(f(x_i)) < tol):
+    return x_i
+
+  return newton_f(x_i)
 
 x = bisect_f(-0.5, 0.5)
 print(x)
 print(f(x))
+
+x = newton_f(0.2)
+print(x)
+print(f(x))
+
