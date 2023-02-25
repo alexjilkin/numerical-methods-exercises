a3ab0faaa5ecbf0395c6f43d99c298ca05f979b9
Alex Jilkin <jilkinalex@gmail.com>
2023-02-25 21:25:08 +0200

    Add nice code results

--- week6/cubic_spline.py
+++ week6/cubic_spline.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 Ns = [5, 10, 20]
 deriv_values = [0, 1, 10]
 
+# Plots cubic spline interpolation for each N and derivative value
 for N in Ns:
   x = np.arange(N)
   y = np.random.uniform(size=N)
 
-
   for deriv_value in deriv_values:
+    # CubicSpline accepts a tuple of tuples of start and end derivatives
     cs = CubicSpline(x, y, bc_type=((2, deriv_value), (2, deriv_value)))
     xs = np.arange(0, N, 0.1)
 
--- week6/richardson.py
+++ week6/richardson.py
@@ -1,26 +1,32 @@
 import numpy as np
 
-N = 6
-h = 0.1
+
 
 def f(x):
-    return np.sin(np.exp(-(x**2)))
+  return np.sin(np.exp(-(x**2)))
 
-D = np.zeros([N + 1, N + 1])
-x = 3
 
-def derivative():
-    hh=h;
 
-    for i in range(N + 1):
-        D[i][0]=(f(x+hh)-f(x-hh))/(2.0*hh)
-    
-        for j in range(i):
-            D[i][j+1] = D[i][j]+(D[i][j]-D[i-1][j])/(pow(4.0,j+1.0)-1.0);
-        hh = hh/2.0;
- 
-derivative()
-h = 1e-5
+def richardson_derivative(x):
+  N = 5
+  h = 0.1
+  D = np.zeros([N + 1, N + 1])
+  hh=h;
 
-print ((f(x + h) - f(x)) / h)
-print(D)
+  for i in range(N + 1):
+    D[i][0]=(f(x+hh)-f(x-hh))/(2.0*hh)
+
+    for j in range(i):
+      D[i][j+1] = D[i][j]+(D[i][j]-D[i-1][j])/(pow(4.0,j+1.0)-1.0)
+    hh = hh/2.0
+  
+  return D[N, N]
+
+
+x = 1
+richardson_res = richardson_derivative(x)
+print("Richardson extrapolation N=5, h=0.1 for f(x): {}".format(richardson_res))
+steps = [10**-4, 10**-6, 10**-8, 10**-10, 10**-12, 10**-14]
+for h in steps:
+  approx_derivative = (f(x + h) - f(x)) / h
+  print("h={}, error={}".format(h, np.abs(richardson_res - approx_derivative)))
