25536da74bcf0e3d19d04f24d4de8943ca193c3f
Alex Jilkin <jilkinalex@gmail.com>
2023-03-30 23:00:03 +0300

    helsinginkatu

--- week10/central_limit.py
+++ week10/central_limit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, cauchy
 import random
 
 Ks = [3, 10, 30, 100]
@@ -9,19 +9,34 @@ Ks = [3, 10, 30, 100]
 # rand: a function returning random number
 def plot_z(rand):
   for K in Ks:
-      # Generate 10^4 mean values of K random numbers
-      z_array = [np.mean([rand() for _ in range(K)]) for _ in range(10**4)]
-      plt.hist(z_array, bins=100, density=True, label="K={}".format(K))
+    # Generate 10^4 mean values of K random numbers
+    z_array = [np.mean([rand() for _ in range(K)]) for _ in range(10**4)]
+    plt.hist(z_array, bins=100, density=True, label="K={}".format(K))
 
-      mu = 0
-      variance = 0.1 / K
-      sigma = np.sqrt(variance)
-      x = np.linspace(-0.5, 0.5, 100)
-      plt.plot(x, norm.pdf(x, mu, sigma), label="var={:.4f}".format(variance))
+# Plots the norm pdf in the range [min, max]
+def plot_norm(mu, var_base, min, max):
+  for K in Ks:
+    # I try to generically fit a norm based on the K size
+    variance = var_base / K
+    sigma = np.sqrt(variance)
+    x = np.linspace(min, max, 100)
+    plt.plot(x, norm.pdf(x, mu, sigma), label="var={:.4f}".format(variance))
 
-  plt.legend()
-  plt.show()
 
-plot_z(lambda: random.uniform(-0.5, 0.5))
+# Uniform distribution
+plot_z(lambda: np.random.uniform(-0.5, 0.5))
+plot_norm(0, 0.1, -0.5, 0.5)
+plt.legend()
+plt.show()
 
-    
+# Exp distribution
+plot_z(lambda: np.random.exponential())
+plot_norm(1, 1, 0, 3)
+plt.legend()
+plt.show()
+
+# Exp distribution
+plot_z(lambda: np.random.standard_cauchy())
+plot_norm(1, 1, 0, 3)
+plt.legend()
+plt.show()
---
+++ week10/helsinginkatu.py
@@ -0,0 +1,78 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+from collections import Counter
+
+d1 = 512
+d2 = 252
+db = 2048
+dl = 512
+
+p_step = 0.05
+
+def flip(p):
+    return True if random.random() < p else False
+
+def is_at_pena(x, n):
+  return x == d2 and flip(1 - 2**(-n/d2)) and flip(2**(-n/dl))
+
+def is_at_home(x, n):
+  # Doesn't miss home with prob 0.9
+  return x == -d1 and flip(0.9)
+
+def is_at_bar(x, n):
+  return x == 0 and flip(1 - 2**(-n/db))
+
+def is_at_tank(x, n):
+  return x == -1154
+
+def is_at_kurvi(x, n):
+  return x == 1154
+
+def simulate():
+  end = False
+  x = 0
+  n = 0
+  direction = 1
+
+  while not end:
+    if (flip(p_step)):
+      direction = direction * -1
+    
+    x += direction
+    n += 1
+
+    if is_at_kurvi(x, n):
+      x -= 1
+    elif (is_at_pena(x, n)):
+      return ("Pena", n)
+    elif (is_at_home(x, n)):
+      return ("Home", n)
+    elif (is_at_bar(x, n)):
+      return ("Bar", n)
+    elif (is_at_tank(x, n)):
+      return ("Tank", n)
+
+simulations = [simulate() for _ in range(3650)]
+print(Counter([place for place, n in simulations]))
+
+samples = {
+  "Pena": [],
+  "Home": [],
+  "Bar": [],
+  "Tank": []
+}
+
+for place, n in simulations:
+  samples[place].append(n)
+
+y = np.sort(samples["Bar"])
+plt.scatter(np.arange(len(y)), y)
+
+plt.show()
+
+
+
+
+
