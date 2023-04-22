0c5260b56aede672ab9615f7436bbc556a9cb2fd
Alex Jilkin <jilkinalex@gmail.com>
2023-04-22 18:20:19 +0300

    Just use specgram...

--- week12/pitch.py
+++ week12/pitch.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.fft import fft, fftfreq
+from scipy.signal import stft
 import matplotlib.pyplot as plt
 
 with open('./samples/sample1.dat') as f:
@@ -10,26 +11,45 @@ samples = samples[1:]
 
 N = len(samples)
 
-# Arange samples around the 0 point
+# Arrange samples around the 0 point
 samples[:, 1] = (samples[:, 1] - 2.5) 
 
 yf = np.abs(fft(samples[:, 1]))
 
-df = samples[1][0] - samples[2][0]
+df = np.abs(samples[1][0] - samples[2][0])
 xf =  np.abs(fftfreq(N, df))
 
-plt.plot(xf, yf)
+# plt.plot(xf, yf)
+# plt.show()
+
+plt.specgram(samples[:, 1], Fs=(1 / df))
 plt.show()
 
-M = int(N / 20)
-window = np.hanning(M)
+# M = int(N / 100)
+# window = np.hanning(M)
+# z = []
+# xf =  np.abs(fftfreq(M, df))
 
-windowed_samples = samples[10 * M: 11 * M] * window
-# windowed_samples[:, 1] = windowed_samples[:, 1] * window
-yf = np.abs(fft(windowed_samples[:, 1]))
-xf =  np.abs(fftfreq(M, df))
-plt.plot(xf, yf)
-plt.show()
+# fft_data = np.array([])
+
+# for i in range(100):
+#     windowed_samples = samples[i * M: (i + 1) * M]
+#     windowed_samples[:, 1] = windowed_samples[:, 1] * window
+
+#     yf = np.abs(fft(windowed_samples[:, 1]))
+    
+#     fft_data = np.concatenate((fft_data, yf))
+
+# fig = plt.figure(figsize = (12, 12))
+ 
+# ax = fig.add_subplot(111, projection='3d')
+# ax.pcolor(np.tile(xf, (100, 1)).flatten(), np.repeat([i for i in range(len(xf))], 100), fft_data, s=1, alpha=1)
+
+# ax.set_xlabel("Freq")
+# ax.set_ylabel("Time")
+# ax.set_zlabel("Amount")
+
+# ax.view_init(10, 90, 0)
 
 # plt.xlim([0, 4000])
-plt.show()
+# plt.show()
