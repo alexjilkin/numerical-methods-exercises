1e5af98aa8e5f7a53a17b99c6a4e002aff1b12ba
Alex Jilkin <jilkinalex@gmail.com>
2023-04-27 23:19:19 +0300

    3 almost working

--- week13/two.py
+++ week13/two.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import legendre
 
 def f(x):
     return np.sin(np.pi * x)
@@ -7,34 +8,43 @@ def f(x):
 def X(j, xi):
     return xi ** j
 
+def X_reg(j, xi):
+    return (xi / 2) ** j
 
-ns = [5, 10, 15, 20, 25]
+def X_leg(j, xi):
+    return legendre(j)((2 * xi) / 4)
 
-for n in ns:
-    x = np.linspace(-2, 2, n)
-   
-    # Builds A matrix
-    A = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            A[i][j] = X(j, x[i])
+def fit(X):
+    ns = [5, 10, 15, 20, 25]
 
-    # Calculates alpha and beta from lecture notes
-    b = f(x)
-    alpha = np.matmul(A.T, A)
-    beta = np.matmul(A.T, b)
+    for n in ns:
+        x = np.linspace(-2, 2, n)
+    
+        # Builds A matrix
+        A = np.zeros((n, n))
+        for i in range(n):
+            for j in range(n):
+                A[i][j] = X(j, x[i])
 
-    # Solves linear equations and builds polynomial
-    sol = np.linalg.solve(alpha, beta)
-    pol = np.polynomial.Polynomial(sol)
+        # Calculates alpha and beta from lecture notes
+        b = f(x)
+        alpha = np.dot(A.T, A)
+        beta = np.dot(A.T, b)
+
+        # Solves linear equations and builds polynomial
+        sol = np.linalg.solve(alpha, beta)
+        pol = np.polynomial.Polynomial(sol)
+
+        x = np.linspace(-2, 2, 200)
+        
+        err = np.linalg.norm(f(x) - pol(x))
+        print(f"n={n} err={err}")
+        plt.plot(x, pol(x), label=f"n={n}")
 
     x = np.linspace(-2, 2, 200)
-    
-    err = np.linalg.norm(f(x) - pol(x))
-    print(f"n={n} err={err}")
-    plt.plot(x, pol(x), label=f"n={n}")
+    plt.plot(x, f(x), label="f")
+    plt.ylim(-3, 3)
+    plt.legend()
+    plt.show()
 
-x = np.linspace(-2, 2, 200)
-plt.plot(x, f(x), label="f")
-plt.legend()
-plt.show()
+fit(X)
