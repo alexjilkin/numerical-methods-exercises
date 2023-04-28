8e7125e143bc36b47d9e79cdbbd8cd09601c55ed
Alex Jilkin <jilkinalex@gmail.com>
2023-04-28 21:17:32 +0300

    Do by file name

--- week13/four.py
+++ week13/four.py
@@ -2,25 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
 
-with open('./data/ex13p4_d1.dat') as f:
+# Reads from the file and creats a matrix with two columns
+fileName = "ex13p4_d3.dat"
+with open(f"./data/{fileName}") as f:
     lines = f.readlines()
 
 samples = np.array([list(map(lambda n: float(n), line.strip().split())) for line in lines])
 
+# Function to fit, getting [a, b] as args. A Maxwell–Boltzmann distribution
 def func(v, args):
     a, b = args
     
     return a * (v**2) * np.exp(-(v**2) / (2*b))
 
+# objective function, returns the residual vector
 def obj_func(args):
     return func(samples[:, 0], args) - samples[:, 1]
 
-args0 = [500, 500]
+# Gets the result by least squares method
+args0 = [100, 100]
 res = least_squares(obj_func, args0, method='lm')
 
-print(res)
+if (res.success != True):
+    print("Failed to fit")
 
-plt.plot(samples[:, 0], samples[:, 1])
+# Plots the data
+plt.plot(samples[:, 0], samples[:, 1], label=fileName)
 
-plt.plot(samples[:, 0], func(samples[:, 0], res.x))
+a, b = res.x
+# Plots found function
+plt.plot(samples[:, 0], func(samples[:, 0], res.x), label=f"a={a:.3f}, b={b:.3f}")
+plt.title(fileName)
+plt.legend()
 plt.show()
